7b67d9023f7adf2b2a5e9976b7673188bb1a6748
akif171 <akifali229@gmail.com>
2024-02-05 23:55:07 +0500

    update pic and bio

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -6,6 +6,9 @@ import Job from "./components/Job";
 export default async function Home() {
   const profile: ProfileType[] = await getProfile();
 
+ 
+  
+
   return (
     <main className="max-w-7xl mx-auto lg:px-16 px-6">
       <section className="flex xl:flex-row flex-col xl:items-center items-start xl:justify-center justify-between gap-x-12 lg:mt-32 mt-20 mb-16">
